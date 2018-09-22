16bed122083e52969feea47b2888ee9bcf86db34
Enrique Rojas <enrique.rojas@riabc.net>
2018-09-21 19:30:34 -0500

    Login

--- CargaClic.API/Migrations/20180921200207_AddedUserEntity.Designer.cs
+++ CargaClic.API/Migrations/20180921200207_AddedUserEntity.Designer.cs
@@ -4,14 +4,16 @@ using CargaClic.API.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace CargaClic.API.Migrations
 {
     [DbContext(typeof(DataContext))]
-    partial class DataContextModelSnapshot : ModelSnapshot
+    [Migration("20180921200207_AddedUserEntity")]
+    partial class AddedUserEntity
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
